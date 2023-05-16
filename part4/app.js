20f09a9d02f32db60f9f2ae6486744aeb9c68e40
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-16 08:12:41 -0300

    refactor

--- part4/app.js
+++ part4/app.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(config.MONGODB_URI, { dbName: 'bloglist' })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
