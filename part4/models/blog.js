582b7709eb93c06ee6d0db53c9e2e48831a2b83b
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-17 10:17:14 -0300

    add constraints and modify mongoose toJSON method

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -2,10 +2,33 @@ const mongoose = require('mongoose')
 const config = require('../utils/config')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+  title: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  author: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  url: {
+    type: String,
+    required: true,
+    minLength: 10
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
