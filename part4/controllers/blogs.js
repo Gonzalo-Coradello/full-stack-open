a9166e434fa2b5411038c73581451e130bbc214e
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-20 10:32:42 -0300

    modify blog router so that only logged in users can create

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,11 +28,17 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const blogData = request.body
-  const user = await User.findOne({})
+  const decodedToken = jwt.verify(getTokenFrom(request), config.JWT_SECRET)
+
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     ...blogData,
-    user: user.id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
