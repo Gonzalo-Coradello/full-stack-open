d333d36cef36c0de487f52e22235007e81fa1b26
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-02 09:11:52 -0300

    add test for adding new anecdotes

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.test.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.test.js
@@ -1,25 +1,22 @@
-import reducer from "./anecdoteReducer";
+import reducer, { voteAnecdote, createAnecdote } from "./anecdoteReducer";
 
 describe('anecdote reducer', () => {
-  test('votes are incremented', () => {
-    const state = [
-      {
-        content: 'If it hurts, do it more often',
-        id: 1,
-        votes: 0
-      },
-      {
-        content: 'Adding manpower to a late software project makes it later!',
-        id: 2,
-        votes: 0
-      }
-    ]
 
-    const action = {
-      type: 'VOTE',
-      payload: { id: 1 }
+  const state = [
+    {
+      content: 'If it hurts, do it more often',
+      id: 1,
+      votes: 0
+    },
+    {
+      content: 'Adding manpower to a late software project makes it later!',
+      id: 2,
+      votes: 0
     }
+  ]
 
+  test('votes are incremented', () => {
+    const action = voteAnecdote(1)
     const newState = reducer(state, action)
     expect(newState).toHaveLength(2)
     expect(newState).toContainEqual({
@@ -27,5 +24,13 @@ describe('anecdote reducer', () => {
       id: 1,
       votes: 1
     })
+  })
+
+  test('a new anecdote can be added', () => {
+    const action = createAnecdote('Any fool can write code that a computer can understand. Good programmers write code that humans can understand.')
+    const newState = reducer(state, action)
+    expect(newState).toHaveLength(3)
+    const anecdotesAtEnd = newState.map(a => a.content)
+    expect(anecdotesAtEnd).toContain('Any fool can write code that a computer can understand. Good programmers write code that humans can understand.')
   })
 })
