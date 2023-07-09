6aa877cbda7e2be0464fa7b48617bc2d2a8721d7
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-09 08:58:53 -0300

    create notification reducer

---
+++ part7/bloglist/frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    set(state, action) {
+      return action.payload
+    },
+    clear() {
+      return ''
+    },
+  },
+})
+
+export const { set, clear } = notificationSlice.actions
+
+export const setNotification = (notification, timeOut) => {
+  const timeMiliseconds = timeOut * 1000
+  return async (dispatch) => {
+    dispatch(set(notification))
+    setTimeout(() => {
+      dispatch(clear())
+    }, timeMiliseconds)
+  }
+}
+
+export default notificationSlice.reducer
