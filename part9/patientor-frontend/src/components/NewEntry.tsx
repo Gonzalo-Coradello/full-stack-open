d794c449f393fa2347942601e47bf5a796bc59aa
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-10-31 07:56:37 -0300

    improve ui and finish application

--- part9/patientor-frontend/src/components/NewEntry.tsx
+++ part9/patientor-frontend/src/components/NewEntry.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { EntryType, EntryWithoutId } from "../types";
+import {
+  Diagnosis,
+  EntryType,
+  EntryWithoutId,
+  HealthCheckRating,
+} from "../types";
 import { useInput } from "../hooks";
 import {
   Box,
@@ -8,8 +13,10 @@ import {
   Select,
   SelectChangeEvent,
   TextField,
+  Typography,
 } from "@mui/material";
 import { assertNever } from "../utils";
+import { diagnosisCodes as allDiagnosisCodes } from "../data/diagnosisCodes";
 
 interface Props {
   handleSubmit: (data: EntryWithoutId) => void;
@@ -18,29 +25,44 @@ interface Props {
 export default function NewEntry({ handleSubmit }: Props) {
   const [entryType, setEntryType] = useState<EntryType>(EntryType.HealthCheck);
   // const [description, setDescription] = useState('');
-  // const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().substring(0, 10));
   // const [specialist, setSpecialist] = useState('');
-  // const [diagnosisCodes, setDiagnosisCodes] = useState('');
-  // const [healthCheckRating, setHealthCheckRating] = useState('');
-  // const [dischargeDate, setDischargeDate] = useState('');
+  const [diagnosisCodes, setDiagnosisCodes] = useState<
+    Array<Diagnosis["code"]>
+  >([]);
+  const [healthCheckRating, setHealthCheckRating] = useState("");
+  const [dischargeDate, setDischargeDate] = useState(
+    new Date().toISOString().substring(0, 10)
+  );
   // const [dischargeCriteria, setDischargeCriteria] = useState('');
   // const [employerName, setEmployerName] = useState('');
-  // const [sickLeaveStart, setSickLeaveStart] = useState('');
-  // const [sickLeaveEnd, setSickLeaveEnd] = useState('');
+  const [sickLeaveStart, setSickLeaveStart] = useState(
+    new Date().toISOString().substring(0, 10)
+  );
+  const [sickLeaveEnd, setSickLeaveEnd] = useState(
+    new Date().toISOString().substring(0, 10)
+  );
 
   const description = useInput("description");
-  const date = useInput("date");
+  // const date = useInput("date");
   const specialist = useInput("specialist");
-  const diagnosisCodes = useInput("diagnosis codes");
-  const healthCheckRating = useInput("health check rating");
-  const dischargeDate = useInput("discharge date");
-  const dischargeCriteria = useInput("discharge criteria");
+  // const diagnosisCodes = useInput("diagnosis codes");
+  // const healthCheckRating = useInput("health check rating");
+  // const dischargeDate = useInput("discharge date");
+  const dischargeCriteria = useInput("criteria");
   const employerName = useInput("employer name");
-  const sickLeaveStart = useInput("sick leave start");
-  const sickLeaveEnd = useInput("sick leave end");
+  // const sickLeaveStart = useInput("sick leave start");
+  // const sickLeaveEnd = useInput("sick leave end");
 
   const handleEntryType = (e: SelectChangeEvent) => {
     setEntryType(e.target.value as EntryType);
+  };
+
+  const handleDiagnosisCodes = (
+    e: SelectChangeEvent<typeof diagnosisCodes>
+  ) => {
+    const value = e.target.value;
+    setDiagnosisCodes(typeof value === "string" ? value.split(",") : value);
   };
 
   const onSubmit = (e: React.SyntheticEvent) => {
@@ -50,10 +72,13 @@ export default function NewEntry({ handleSubmit }: Props) {
         const healthCheckEntry: EntryWithoutId = {
           type: entryType,
           description: description.value,
-          date: date.value,
+          date: date,
           specialist: specialist.value,
-          diagnosisCodes: diagnosisCodes.value.split(",").map((v) => v.trim()),
-          healthCheckRating: +healthCheckRating.value,
+          diagnosisCodes,
+          healthCheckRating:
+            HealthCheckRating[
+              healthCheckRating as keyof typeof HealthCheckRating
+            ],
         };
         handleSubmit(healthCheckEntry);
         break;
@@ -61,11 +86,11 @@ export default function NewEntry({ handleSubmit }: Props) {
         const hospitalEntry: EntryWithoutId = {
           type: entryType,
           description: description.value,
-          date: date.value,
+          date: date,
           specialist: specialist.value,
-          diagnosisCodes: diagnosisCodes.value.split(",").map((v) => v.trim()),
+          diagnosisCodes,
           discharge: {
-            date: dischargeDate.value,
+            date: dischargeDate,
             criteria: dischargeCriteria.value,
           },
         };
@@ -75,13 +100,13 @@ export default function NewEntry({ handleSubmit }: Props) {
         const occupationalEntry: EntryWithoutId = {
           type: entryType,
           description: description.value,
-          date: date.value,
+          date: date,
           specialist: specialist.value,
-          diagnosisCodes: diagnosisCodes.value.split(",").map((v) => v.trim()),
+          diagnosisCodes,
           employerName: employerName.value,
           sickLeave: {
-            startDate: sickLeaveStart.value,
-            endDate: sickLeaveEnd.value,
+            startDate: sickLeaveStart,
+            endDate: sickLeaveEnd,
           },
         };
         handleSubmit(occupationalEntry);
@@ -96,44 +121,102 @@ export default function NewEntry({ handleSubmit }: Props) {
       <Box border={1} p={3}>
         <h2>New entry</h2>
         <form onSubmit={onSubmit}>
-          <Box display="grid" gap={1.5}>
-            <Select
-              onChange={handleEntryType}
-              defaultValue={EntryType.HealthCheck}
-              variant="standard"
-            >
-              <MenuItem value={EntryType.HealthCheck}>Health check</MenuItem>
-              <MenuItem value={EntryType.Hospital}>Hospital</MenuItem>
-              <MenuItem value={EntryType.OccupationalHealthcare}>
-                Occupational health care
-              </MenuItem>
-            </Select>
+          <Box display="grid" gap={2.5}>
+            <Box display="grid" gap={1}>
+              <Typography color="GrayText">Entry type</Typography>
+              <Select
+                onChange={handleEntryType}
+                defaultValue={EntryType.HealthCheck}
+                variant="standard"
+              >
+                <MenuItem value={EntryType.HealthCheck}>Health check</MenuItem>
+                <MenuItem value={EntryType.Hospital}>Hospital</MenuItem>
+                <MenuItem value={EntryType.OccupationalHealthcare}>
+                  Occupational health care
+                </MenuItem>
+              </Select>
+            </Box>
             <TextField variant="standard" type="text" {...description} />
-            <TextField variant="standard" type="text" {...date} />
+            <TextField
+              variant="standard"
+              type="date"
+              label="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
             <TextField variant="standard" type="text" {...specialist} />
-            <TextField variant="standard" type="text" {...diagnosisCodes} />
+            <Box display="grid" gap={1}>
+              <Typography color="GrayText">Diagnosis codes</Typography>
+              <Select
+                variant="standard"
+                multiple
+                onChange={handleDiagnosisCodes}
+                value={diagnosisCodes}
+              >
+                {allDiagnosisCodes.map((code) => (
+                  <MenuItem key={code} value={code}>
+                    {code}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Box>
             {entryType === "HealthCheck" ? (
-              <>
-                <TextField
+              <Box display="grid" gap={1}>
+                <Typography color="GrayText">Health check rating</Typography>
+                <Select
                   variant="standard"
-                  type="text"
-                  {...healthCheckRating}
-                />
-              </>
+                  onChange={(e) => setHealthCheckRating(e.target.value)}
+                  value={healthCheckRating}
+                >
+                  {Object.keys(HealthCheckRating)
+                    .filter((v) => isNaN(+v))
+                    .map((rating) => (
+                      <MenuItem key={rating} value={rating}>
+                        {rating}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </Box>
             ) : entryType === "Hospital" ? (
-              <>
-                <TextField variant="standard" type="text" {...dischargeDate} />
-                <TextField
-                  variant="standard"
-                  type="text"
-                  {...dischargeCriteria}
-                />
-              </>
+              <Box display="grid" gap={1}>
+                <Typography color="GrayText">Discharge</Typography>
+                <Box display="flex" gap={3}>
+                  <TextField
+                    variant="standard"
+                    type="date"
+                    label="date"
+                    value={dischargeDate}
+                    onChange={(e) => setDischargeDate(e.target.value)}
+                  />
+                  <TextField
+                    variant="standard"
+                    type="text"
+                    {...dischargeCriteria}
+                  />
+                </Box>
+              </Box>
             ) : entryType === "OccupationalHealthcare" ? (
               <>
                 <TextField variant="standard" type="text" {...employerName} />
-                <TextField variant="standard" type="text" {...sickLeaveStart} />
-                <TextField variant="standard" type="text" {...sickLeaveEnd} />
+                <Box display="grid" gap={1}>
+                  <Typography color="GrayText">Sickleave</Typography>
+                  <Box display="flex" gap={3}>
+                    <TextField
+                      variant="standard"
+                      type="date"
+                      label="start"
+                      value={sickLeaveStart}
+                      onChange={(e) => setSickLeaveStart(e.target.value)}
+                    />
+                    <TextField
+                      variant="standard"
+                      type="date"
+                      label="end"
+                      value={sickLeaveEnd}
+                      onChange={(e) => setSickLeaveEnd(e.target.value)}
+                    />
+                  </Box>
+                </Box>
               </>
             ) : null}
             <Button
